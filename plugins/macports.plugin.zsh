88e3983531da52168fa01ff7fcd97dc37f099c4b
Flavio Castelli <flavio@castelli.name>
2010-08-25 22:13:06 +0200

    added some plugins I need under osx

---
+++ plugins/macports.plugin.zsh
@@ -0,0 +1,2 @@
+export MANPATH=/opt/local/share/man:$MANPATH
+export PATH=/opt/local/bin:$PATH
---
+++ plugins/mbp-keyboard.plugin.zsh
@@ -0,0 +1,6 @@
+bindkey "^[[5~" up-line-or-history
+bindkey "^[[6~" down-line-or-history
+bindkey "^[[H" beginning-of-line
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[F"  end-of-line
+bindkey "^[[4~" end-of-line
